922a88ce360040b9edf1193d67c29ed2e93aa3f1
Paulo Cezar de Souza Filho <Paulo.2012@hotmail.com>
2021-12-26 17:19:18 -0300

    Pop-up com 30% do design pronto, já é possivel mudar ele de lugar

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const fetchPokemon = () => {
       accumulator += `<tr><th onclick="info()">${pokemon.id}. ${
         pokemon.name
       }</th></tr><tr>
-        <td >${pokemon.types
+        <td>${pokemon.types
           .map((typeInfo) => typeInfo.type.name)
           .join(" | ")}</td></tr>`;
       return accumulator;
@@ -26,8 +26,59 @@ const fetchPokemon = () => {
   });
 };
 function info() {
-  alert(
-    "isso aqui vai ser um popup com peso, altura, descrição e evolucção do pokemon"
-  );
+  document.getElementById("popup").style.display = "block";
+}
+function close() {
+  document.getElementById("popup").style.display = "none";
 }
 fetchPokemon();
+
+window.onload = function () {
+  Dragable(document.getElementById("popup"));
+};
+
+//Adiciona eventos pra navegadores modernos e antigos
+function addEvent(el, type, callback) {
+  if (el.addEventListener) {
+    el.addEventListener(type, callback);
+  } else if (el.attachEvent) {
+    el.attachEvent("on" + type, callback);
+  }
+}
+
+function Dragable(el) {
+  var isMove = false, //Verifica se esta se movendo
+    x = 0,
+    y = 0, //Pega as coordenadas do mouse
+    xel = 0,
+    yel = 0; // ultima posição do elemento
+
+  addEvent(el, "mousedown", function (e) {
+    isMove = true;
+
+    el.className += " isMoving";
+
+    x = window.event ? window.event.clientX : e.pageX;
+    y = window.event ? window.event.clientY : e.pageY;
+
+    xel = x - el.offsetLeft;
+    yel = y - el.offsetTop;
+  });
+
+  addEvent(document, "mousemove", function (e) {
+    if (isMove) {
+      e.preventDefault();
+
+      x = window.event ? window.event.clientX : e.pageX;
+      y = window.event ? window.event.clientY : e.pageY;
+
+      el.style.left = x - xel + "px";
+      el.style.top = y - yel + "px";
+    }
+  });
+
+  addEvent(document, "mouseup", function () {
+    el.className = String(el.className).replace(/(^|\s)isMoving(\s|$)/g, " ");
+    isMove = false;
+  });
+}
